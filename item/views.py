9bc2ea2a6065cf27907e24efe66074e5b8a3bf3d
masoudkazak <kzmasut@gmail.com>
2021-12-19 11:10:28 +0330

    add-comment-item

--- item/views.py
+++ item/views.py
@@ -1,4 +1,5 @@
-from .models import Item
+from django.http import HttpResponseForbidden
+from .models import Comment, Item
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,8 +9,8 @@ from django.views.generic import (
 )
 from .forms import ItemUpdateForm, CommentForm
 from django.urls import reverse
-from django.views.generic.edit import FormMixin
-from django.http import HttpResponseForbidden
+from django.views.generic.edit import ModelFormMixin
+from django.shortcuts import redirect
 
 
 class ItemListView(ListView):
@@ -18,27 +19,27 @@ class ItemListView(ListView):
     template_name = 'home.html'
 
 
-class ItemDetailView(FormMixin ,DetailView):
+class ItemDetailView(ModelFormMixin ,DetailView):
     model = Item
     context_object_name = 'item'
     template_name = 'itemdetail.html'
     form_class = CommentForm
-
-    def get_success_url(self):
-        return reverse('item:detail', kwargs={'pk': self.object.pk})
     
     def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return HttpResponseForbidden
-        self.object = self.get_object()
+            return HttpResponseForbidden()
+        
         form = self.get_form()
         if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+            text = form.cleaned_data['text']
+            item = self.get_object()
+            user = request.user
+            new_comment = Comment(text=text,
+                                item=item,
+                                user=user)
+            new_comment.save()
+            return redirect('item:detail', pk=item.pk)
     
-    def form_valid(self, form):
-        return super().form_valid(form)
 
 
 class ItemUpdateView(UpdateView):
