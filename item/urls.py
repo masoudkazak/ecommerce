09077fcd136b1435c5bc5dc5cd475bda60d730c6
masoudkazak <kzmasut@gmail.com>
2021-12-27 20:03:10 +0330

    startapp-blog

--- item/urls.py
+++ item/urls.py
@@ -13,8 +13,8 @@ app_name = 'item'
 urlpatterns = [
     path('', ItemListView.as_view(), name='list'),
     path('item/create/', ItemCreateView.as_view(), name='create'),
-    path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
-    path('detail/<int:pk>/update/', ItemUpdateView.as_view(), name='update'),
-    path('detail/<int:pk>/delete/', ItemDeleteView.as_view(), name='delete'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='detail'),
+    path('item/<int:pk>/update/', ItemUpdateView.as_view(), name='update'),
+    path('item/<int:pk>/delete/', ItemDeleteView.as_view(), name='delete'),
     
 ]
--- masoud/settings.py
+++ masoud/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'account',
+    'blog',
 ]
 
 MIDDLEWARE = [
